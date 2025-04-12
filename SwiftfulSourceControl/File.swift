dea3cef789f5e86863ee8755700f81edad791ddd
Star <1647490509@qq.com>
2025-04-12 16:49:56 +0800

    测试

---
+++ SwiftfulSourceControl/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  SwiftfulSourceControl
+//
+//  Created by Outsider on 2025/4/12.
+//
+
+import Foundation
+class asd {
+    let id: Int
+    let name:String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
