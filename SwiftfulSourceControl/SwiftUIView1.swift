02ade1c1943b05d474ed64f40f68463f7e298781
Star <1647490509@qq.com>
2025-04-12 16:10:25 +0800

    添加视图

---
+++ SwiftfulSourceControl/SwiftUIView1.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView1.swift
+//  SwiftfulSourceControl
+//
+//  Created by Outsider on 2025/4/12.
+//
+
+import SwiftUI
+
+struct SwiftUIView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView1()
+}
